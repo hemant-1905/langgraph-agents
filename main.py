1e508e990e50269e01f93aaa2a4d4f20ff1aa6fc
Hemant Sharma <hemantsharma@Hemants-MacBook-Air.local>
2026-07-03 16:31:04 +0530

    Adding langsmith tracing

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import os
+
 from dotenv import load_dotenv
 load_dotenv()
 
 from graph.graph import app
 from graph import graph
+import os
+
+os.environ["LANGCHAIN_ENDPOINT"] = "https://eu.api.smith.langchain.com"
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
 
 if __name__ == "__main__":
     print("Starting the LangGraph application...")
-    print(app.invoke({"question": "agent memory"}))
+    print(app.invoke({"question": "who are the top runners for fifa wc 2026?"}))
